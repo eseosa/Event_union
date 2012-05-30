299ed4025ce6e00904c839ee842b1f4b67c19ec8
Eseosa Oshodin <eseosa.oshodin.10@googlemail.com>
2012-05-30 22:38:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventsUnion::Application.config.secret_token = 'd0b95604885b9e2a69caed624185e13fbe139966c303a84820d4817932fb8ee9a2dd398dd89a0a93ecf55dcae53d16c6c6842f6cf11b87628fcc541716b8fe48'
